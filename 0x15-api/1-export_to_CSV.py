3860d7f47fa446ded57576dcc60209afc5875c9b
saad-out <outsaad03@gmail.com>
2023-02-08 12:16:37 +0000

    use old code to test docs

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,23 +1,39 @@
 #!/usr/bin/python3
 """Request employee ID from API
 """
+
 import csv
 import requests
-import sys
+from sys import argv
 
-if __name__ == '__main__':
-    user_id = int(sys.argv[1])
-    user = requests.get('https://jsonplaceholder.typicode.com/users/' +
-                        '{}'.format(user_id))
-    todos = requests.get('https://jsonplaceholder.typicode.com/todos' +
-                         '?userId={}'.format(user_id))
+if __name__ == "__main__":
 
-    with open(f"{user_id}.csv", 'w', encoding='UTF8') as f:
-        writer = csv.writer(f, quoting=csv.QUOTE_ALL)
-        row = [user_id, user.json().get("username")]
-        for task in todos.json():
-            row.append(task.get("completed"))
-            row.append(task.get("title"))
-            writer.writerow(row)
-            row.pop()
-            row.pop()
+    def make_request(resource, param=None):
+        """Retrieve user from API
+        """
+        url = 'https://jsonplaceholder.typicode.com/'
+        url += resource
+        if param:
+            url += ('?' + param[0] + '=' + param[1])
+
+        # make request
+        r = requests.get(url)
+
+        # extract json response
+        r = r.json()
+        return r
+
+    user = make_request('users', ('id', argv[1]))[0]
+    tasks = make_request('todos', ('userId', argv[1]))
+
+    csv_filename = argv[1] + '.csv'
+    with open(csv_filename, mode='w') as f:
+        writer = csv.writer(f,
+                            delimiter=',',
+                            quotechar='"',
+                            quoting=csv.QUOTE_ALL)
+        for task in tasks:
+            writer.writerow([user['id'],
+                            user['username'],
+                            task['completed'],
+                            task['title']])
