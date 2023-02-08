d4fd72cdcb91b5bcccd55771a2eb6954c1c1d35f
saad-out <outsaad03@gmail.com>
2023-02-08 11:58:45 +0000

    write to csv file

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -10,6 +10,7 @@ def main():
     Returns all tasks owned by an employee based on ID in CSV format
     using a REST API
     """
+    import csv
     import requests
     import sys
 
@@ -19,11 +20,15 @@ def main():
     todos = requests.get('https://jsonplaceholder.typicode.com/todos' +
                          '?userId={}'.format(user_id))
 
-    for task in todos.json():
-        print('"{}","{}","{}","{}"'.format(user_id,
-                                           user.json().get('username'),
-                                           task.get('completed'),
-                                           task.get('title')))
+    with open(f"{user_id}.csv", 'w', encoding='UTF8') as f:
+        writer = csv.writer(f, quoting=csv.QUOTE_ALL)
+        row = [user_id, user.json().get("username")]
+        for task in todos.json():
+            row.append(task.get("completed"))
+            row.append(task.get("title"))
+            writer.writerow(row)
+            row.pop()
+            row.pop()
 
 
 if __name__ == '__main__':
